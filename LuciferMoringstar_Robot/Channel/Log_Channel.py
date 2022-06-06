ef3463e4151de23bc9f4d12e0dd3c9c7f632a219
SydXyx <106866376+SydXyx@users.noreply.github.com>
2022-06-06 08:48:39 +0530

    Update Log_Channel.py

--- LuciferMoringstar_Robot/Channel/Log_Channel.py
+++ LuciferMoringstar_Robot/Channel/Log_Channel.py
@@ -26,6 +26,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You are Banned to Use This Bot ", quote=True)
+            await cmd.reply_text("🤬😏😒 ", quote=True)
             return
     await cmd.continue_propagation()
